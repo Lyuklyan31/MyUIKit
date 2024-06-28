84f0490230fc6771af32f1d8931bb9091ca59fd6
oleg <lyuclyan.oleg14@gmail.com>
2024-06-28 18:37:32 +0300

    UIButton

--- UIButton/UIButton/ViewController.swift
+++ UIButton/UIButton/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    @IBAction func pushMeActiona(_ sender: Any) {
+        label.text = "You push me"
+        button.setTitle("You are guy", for: .normal)
+        button.isEnabled = false
+        button.setTitleColor(UIColor.gray, for: .normal)
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
