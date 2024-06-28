5d2f6f058a3b314fdab82369be84303afb6ba8b5
oleg <lyuclyan.oleg14@gmail.com>
2024-06-28 17:31:05 +0300

    UILable

--- UILabel/UILabel/ViewController.swift
+++ UILabel/UILabel/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        label.text = "Hello, World!"
+        
+        label.textColor = UIColor.blue
+        label.backgroundColor = UIColor.red
     }
 
 
