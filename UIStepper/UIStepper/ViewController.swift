dcab770572d23d72217437da9e38aed6f7eadb97
oleg <lyuclyan.oleg14@gmail.com>
2024-07-03 00:40:42 +0300

    UIStepper

--- UIStepper/UIStepper/ViewController.swift
+++ UIStepper/UIStepper/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  UIStepper
-//
-//  Created by admin on 03.07.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stepperMain: UIStepper!
+    @IBOutlet weak var stepperStep: UIStepper!
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var labelStep: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func stepperMainAction(_ sender: Any) {
+        textField.text = "\(stepperMain.value)"
+    }
+    
+    @IBAction func stepperStepAction(_ sender: Any) {
+        labelStep.text = "Step: \(stepperStep.value)"
+        stepperMain.stepValue = stepperStep.value
+    }
+    
+    @IBAction func textFieldAction(_ sender: Any) {
+        stepperMain.value = Double(Int(textField.text!) ?? 0)
+    }
 }
 
