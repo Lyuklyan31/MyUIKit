36960d2af43b266b584e96903ee676217bb04255
oleg <lyuclyan.oleg14@gmail.com>
2024-07-02 13:56:40 +0300

    UITextView

--- UITextView/UITextView/ViewController.swift
+++ UITextView/UITextView/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textView.delegate = self
     }
-
+    func textViewDidChange(_ textView: UITextView) {
+        print(textView.text)
+        
+        if textView.text == "exit" {
+            textView.resignFirstResponder()
+        }
+    }
 
 }
 
