5512e6fc0442210eb9a5d4ef564ca1554a98060b
oleg <lyuclyan.oleg14@gmail.com>
2024-07-03 01:22:36 +0300

    AlertController

--- UIAlertController/UIAlertController/ViewController.swift
+++ UIAlertController/UIAlertController/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pushMeAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Hello", message: "Massage here", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        
+        present(alertController, animated: true)
+    }
+    
 }
 
