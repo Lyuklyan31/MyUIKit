52306e194321a44899e9b3df7f3e8de936495ff5
oleg <lyuclyan.oleg14@gmail.com>
2024-07-02 13:24:42 +0300

    no message

---
+++ UITextField/UITextField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextField
+//
+//  Created by admin on 28.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
