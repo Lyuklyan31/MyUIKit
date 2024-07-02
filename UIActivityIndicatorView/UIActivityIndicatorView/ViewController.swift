d84e0e1a0941500c49518b875540d78e1b8bd848
oleg <lyuclyan.oleg14@gmail.com>
2024-07-03 01:16:58 +0300

    UIActivityIndicatorView

--- UIActivityIndicatorView/UIActivityIndicatorView/ViewController.swift
+++ UIActivityIndicatorView/UIActivityIndicatorView/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  UIActivityIndicatorView
-//
-//  Created by admin on 03.07.2024.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    var indicator2: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        indicator2 = UIActivityIndicatorView(style: .medium)
+        indicator2.hidesWhenStopped = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: indicator2)
     }
 
-
+    @IBAction func pushMeAction(_ sender: Any) {
+        indicator.startAnimating()
+        indicator2.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.indicator.stopAnimating()
+            self.indicator2.stopAnimating()
+        }
+    }
+    
 }
 
