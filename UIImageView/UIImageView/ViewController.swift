71be262049f7a98a90e2cb9f2450c7d5bbe46c60
oleg <lyuclyan.oleg14@gmail.com>
2024-07-02 21:53:02 +0300

    UIImageView

--- UIImageView/UIImageView/ViewController.swift
+++ UIImageView/UIImageView/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func sliderAction(_ sender: Any) {
+        imageView.alpha = CGFloat(slider.value)
+    }
+    
+    @IBAction func pushMeButtonAction(_ sender: Any) {
+        imageView.image = UIImage(named: "imagetest.jpeg")
+    }
+    
 }
 
