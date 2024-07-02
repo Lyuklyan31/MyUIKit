ac2c8a82549aee02f5b3aa31ba83704d14f4de84
oleg <lyuclyan.oleg14@gmail.com>
2024-07-02 19:57:54 +0300

    no message

---
+++ Switch/Switch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Switch
+//
+//  Created by admin on 02.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
