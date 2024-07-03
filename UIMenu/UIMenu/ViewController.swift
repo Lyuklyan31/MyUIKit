05d9e558ca430de55465322e51708f02ff510254
oleg <lyuclyan.oleg14@gmail.com>
2024-07-03 18:24:02 +0300

    UIMenu

--- UIMenu/UIMenu/ViewController.swift
+++ UIMenu/UIMenu/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var menuItemButton: UIBarButtonItem!
+    @IBOutlet weak var menuButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let action1 = UIAction(title: "Action1") { _ in
+        print("action1")
+        }
+        
+        let action2 = UIAction(title: "Action2") { _ in
+        print("action2")
+        }
+        
+        let menu = UIMenu(title: "Main menu", children: [action1, action2])
+        
+        menuItemButton.menu = menu
+        menuButton.menu = menu
+        menuButton.showsMenuAsPrimaryAction = true
     }
-
-
+    @IBAction func pushMeItemAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pushMenuAction(_ sender: Any) {
+    }
+    
 }
 
