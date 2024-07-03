0f8173b9c37d969c4a7cd065d34ad0c5fbbecb37
oleg <lyuclyan.oleg14@gmail.com>
2024-07-03 12:00:16 +0300

    UIActivityViewController

--- UIActivityViewController/UIActivityViewController/ViewController.swift
+++ UIActivityViewController/UIActivityViewController/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func pushShareAction(_ sender: Any) {
+        let text = textView.text
+        let avc = UIActivityViewController(activityItems: [text], applicationActivities: nil)
+        present(avc, animated: true)
+    }
+    
 
 }
 
