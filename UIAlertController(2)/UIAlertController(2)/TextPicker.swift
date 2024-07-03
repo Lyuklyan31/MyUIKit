5b5517fe11fbf417a4640331a7136857a8e076fd
oleg <lyuclyan.oleg14@gmail.com>
2024-07-03 11:53:31 +0300

    UIAlertController2

--- UIAlertController(2)/UIAlertController(2)/TextPicker.swift
+++ UIAlertController(2)/UIAlertController(2)/TextPicker.swift
@@ -4,5 +4,30 @@
 //
 //  Created by admin on 03.07.2024.
 //
-
+import UIKit
 import Foundation
+
+class TextPicker {
+    
+    static let defaultPicker = TextPicker()
+    
+    func getText(viewController: UIViewController, competion: @escaping (_ text: String)->()) {
+        let alertController = UIAlertController(title: "Entertext", message: nil, preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "Enter text"
+        }
+        
+        
+        let actionOK = UIAlertAction(title: "OK", style: .default) { action in
+            let text = alertController.textFields![0].text!
+            competion(text)
+        }
+        
+        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(actionOK)
+        alertController.addAction(actionCancel)
+        
+        viewController.present(alertController, animated: true)
+    }
+}
--- UIAlertController(2)/UIAlertController(2)/ViewController.swift
+++ UIAlertController(2)/UIAlertController(2)/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func pushEnterTextAction(_ sender: Any) {
+        TextPicker.defaultPicker.getText(viewController: self) { text in
+            self.label.text = text
+        }
+    }
+    
 
 }
 
